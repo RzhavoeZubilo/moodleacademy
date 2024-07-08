6d6f64687893f992d3bef3af82c20df18a1c279f
Денис Шкуропат <denshkuropat@gmail.com>
2024-07-08 15:54:28 +0300

    Плагин Фильтр

---
+++ filter/textsubstitute/filter.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Filter to substitute text.
+ *
+ * @package     filter_textsubstitute
+ * @category    admin
+ * @copyright   2023 Your Name <you@example.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class filter_textsubstitute extends moodle_text_filter {
+    /**
+     * Apply the filter to the text
+     *
+     * @see filter_manager::apply_filter_chain()
+     * @param string $text to be processed by the text
+     * @param array $options filter options
+     * @return string text after processing
+     */
+    public function filter($text, array $options = []) {
+        $searchterm = 'Moodle';
+        $replacewith = 'UniLearn';
+        $text = str_replace($searchterm, $replacewith, $text);
+        // Возвращает измененный текст.
+        return $text;
+    }
+}
